1f00bc634ba0ffa39aee2effaa837d4a5d43582f
Murlidhar Carpenter <134859656+murlivishwakarma@users.noreply.github.com>
2025-11-18 13:28:59 +0530

    Update interview.route.js

--- src/routes/interview.route.js
+++ src/routes/interview.route.js
@@ -11,8 +11,8 @@ import {
 import { verifyJWT } from "../middlewares/Backend/src/middlewares/auth.middleware.js";
 const app = Router();
 
-//app.post("/schedule-interview", verifyJWT, scheduleInterview);
-app.post("/schedule-interview", scheduleInterview);
+app.post("/schedule-interview", verifyJWT, scheduleInterview);
+//app.post("/schedule-interview", scheduleInterview);
 
 app.get("/view-interviews-for-interviewer/:id", verifyJWT, getInterviews);
 
@@ -28,4 +28,5 @@ app.get("/markascomplete/:interviewId", verifyJWT, markAsCompleted);
 app.post("/getresult/:interviewId", verifyJWT, getResultByInterviewId);
 
 export default app;
+
 
